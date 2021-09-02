a091504b07386910579ef86bc3cf8d185368de7f
H37kouya <h37kouya@gmail.com>
2021-09-02 14:14:09 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 樋口航也 on 2021/09/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/DemoDefaultXcodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoDefaultXcodeAppApp.swift
+//  Shared
+//
+//  Created by 樋口航也 on 2021/09/02.
+//
+
+import SwiftUI
+
+@main
+struct DemoDefaultXcodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
